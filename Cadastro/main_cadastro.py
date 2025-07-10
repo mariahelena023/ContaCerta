23cec3cb14c61cf471ccb95d77d7d205cfe690ca
mariaaa <toledomiranda.maria23@gmail.com>
2025-07-10 14:50:38 -0400

    página de cadastros
    os cadastros serão salvos em um arquivo .json, só falta mexer no botão de vvoltar para ser encaminhado para página inicial

--- Cadastro/main_cadastro.py
+++ Cadastro/main_cadastro.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import json
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 from cadastro import Ui_MainWindow
 
@@ -8,30 +10,35 @@ class MainCadastro(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.arquivo_cadastros = 'cadastrosSalvos/cadastrosSalvos.json'
+        self.salvar_cadastros = {}
+        
         self.ui.pushButton_Cadastrar.clicked.connect(self.cadastrar)
         self.ui.pushButton_Cancelar.clicked.connect(self.cancelar)
-        self.ui.pushButton_Voltar.clicked.connect(self.voltar)
+        # self.ui.pushButton_Voltar.clicked.connect(self.voltar)
         
 
     def cadastrar (self):
         nome = self.ui.lineEdit_Nome.text()
         senha = self.ui.lineEdit_Senha.text()
         confirmar_senha = self.ui.lineEdit_ConfirmarSenha.text()
+
+        self.salvar_cadastros[nome] = {
+            "Senha" : senha,
+            "Confirmação de Senha" : confirmar_senha
+        }
+
+        self.salvar_cadastro_json()
+        QMessageBox.information(self, 'Sucesso', 'Dados salvos com sucesso!')
+
+    def salvar_cadastro_json(self):
+        try:
+            os.makedirs(os.path.dirname(self.arquivo_cadastros), exist_ok=True)
+            with open(self.arquivo_cadastros, 'w') as f:
+                json.dump(self.salvar_cadastros, f, indent=4)
+        except Exception as e:
+            QMessageBox.critical(self, 'Erro ao salvar!', f'Ocorreu um erro ao salvar os dados:\n{str(e)}')
         
-        if not nome and senha and confirmar_senha:
-            QMessageBox.warning(self, "Atenção", "Por favor, preencha o campo.")
-            return
-
-        conteudo = f"Nome: {nome}\nSenha:{senha}\nSenha Confirmada? {confirmar_senha}"
-
-        caminho = QFileDialog.getSaveFileName(self, "Salvar", "", "Text Files (*.txt)")[0]
-        if caminho:
-            try:
-                with open(caminho, 'w', encoding='utf-8') as f:
-                    f.write(conteudo)
-                QMessageBox.information(self, "Sucesso", "Dados salvos com sucesso!")
-            except Exception as e:
-                QMessageBox.critical(self, "Erro", f"Erro ao salvar arquivo:\n{str(e)}")
 
 
 
@@ -41,8 +48,8 @@ class MainCadastro(QMainWindow):
         self.ui.lineEdit_ConfirmarSenha.clear()
 
 
-    def voltar (self):
-        caminho = 
+    # def voltar (self):
+    #     caminho = 
         
      
 
