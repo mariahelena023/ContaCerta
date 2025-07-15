921f850cdaf8fff61c1b9ef410b9b7c9eb9fcd5a
mariaaa <toledomiranda.maria23@gmail.com>
2025-07-15 14:54:36 -0400

    main_login.py criado

---
+++ Login/main_login.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import json
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+from login import Ui_MainWindow
+
+class MainLogin(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton_Login.clicked.connect(self.login)
+        self.ui.pushButton_CancelarLogin.clicked.connect(self.cancelar)
+        self.ui.pushButton_voltarLogin.clicked.connect(self.voltar)
+        
+
+        
+     
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainLogin()
+    window.show()
+    sys.exit(app.exec_())
