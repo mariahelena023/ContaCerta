174c8fdd3a0b587327988f66019517fa06e3705d
mariaaa <toledomiranda.maria23@gmail.com>
2025-07-21 14:34:03 -0400

    arrumando arquivos

--- TelaInicial/main_tela_inicial.py
+++ TelaInicial/main_tela_inicial.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from tela_incial import Ui_MainWindow
-from ..Cadastro import main_cadastro
+from Cadastro import main_cadastro
+from Login import main_login
 
 class MainTelaInicial(QMainWindow):
     def __init__(self):
@@ -9,28 +10,16 @@ class MainTelaInicial(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-
-        # Conectar os botões às funções
         self.ui.login_button.clicked.connect(self.abrir_tela_login)
         self.ui.login_button_2.clicked.connect(self.abrir_tela_cadastro)
 
     def abrir_tela_login(self):
-        # Esconder a janela atual
         self.hide()
-
-    #     # Criar e mostrar a tela de login
-        self.tela_login = TelaLogin()
+        self.tela_login = main_login()
         self.tela_login.show()
 
     def abrir_tela_cadastro(self):
-        # Esconder a janela atual
         self.hide()
-
-        # Criar e mostrar a tela de cadastro
         self.tela_cadastro = main_cadastro()
         self.tela_cadastro.show()
 
