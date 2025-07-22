41ed124e3af1c9750f16593e91ac2bb403c846a6
maria <toledomiranda.maria23@gmail.com>
2025-07-22 09:20:08 -0300

    arrumando tela inicial

--- TelaInicial/main_tela_inicial.py
+++ TelaInicial/main_tela_inicial.py
@@ -2,8 +2,8 @@ import sys
 import os
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from tela_inicial import Ui_MainWindow
-from Login.login import MainLogin
-from Cadastro.cadastro import MainCadastro
+# from Login.main_login import MainLogin
+# from Cadastro.main_cadastro import MainCadastro
 
 class MainTelaInicial(QMainWindow):
     def __init__(self):
@@ -11,18 +11,18 @@ class MainTelaInicial(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.ui.login_button.clicked.connect(self.abrir_tela_login)
-        self.ui.login_button_2.clicked.connect(self.abrir_tela_cadastro)
+        # self.ui.login_button.clicked.connect(self.abrir_tela_login)
+        # self.ui.login_button_2.clicked.connect(self.abrir_tela_cadastro)
 
-    def abrir_tela_login(self):
-        self.hide()
-        self.tela_login = MainLogin()
-        self.tela_login.show()
+    # def abrir_tela_login(self):
+    #     self.hide()
+    #     self.tela_login = MainLogin()
+    #     self.tela_login.show()
 
-    def abrir_tela_cadastro(self):
-        self.hide()
-        self.tela_cadastro = MainCadastro()
-        self.tela_cadastro.show()
+    # def abrir_tela_cadastro(self):
+    #     self.hide()
+    #     self.tela_cadastro = MainCadastro()
+    #     self.tela_cadastro.show()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
